c466ad906b41e666d6e992e2c5d89688a4612f5f
Yushenko Dima <damios.1812@gmail.com>
2023-01-30 15:27:41 +0200

    first commit2

--- js/app.js
+++ js/app.js
@@ -200,6 +200,10 @@ function stopRotateWheel() {
   box.classList.toggle("no-click");
 }
 
+function $_GET(e,t){return!!(t=t.match(new RegExp(e+"=([^&=]+)")))&&t[1]}
+var getStag = $_GET("stag", window.location.href);
+var stag = "?stag=" + getStag;
+console.log("STAG -" + stag);
 
 $('.btn-1').click(function(){
   $('body').addClass('two')
@@ -208,8 +212,14 @@ $('.btn-1').click(function(){
 
 })
 
+$('.btn-2').click(function(){     
+    window.location.href = 'https://arlekincasino.net/en-CA/users/sign_up' + stag;
+})
+
+
+
 $('.btn-2').click(function(){
-  window.location.href = 'https://www.casinokakadu.com/users/sign_up';
+  window.location.href = 'https://www.casinokakadu.com/users/sign_up' + stag;
 })
 
 function easeOut(t, b, c, d) {
